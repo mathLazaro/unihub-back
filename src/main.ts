bbb52c8311bcac41f1bf47123d700cbb8fa00f3d
Matheus Lázaro <matheus.lazaro2002@gmail.com.br>
2026-06-18 00:13:22 -0300

    fix: ajuste cors

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,11 @@ async function bootstrap() {
     )
     .build();
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
